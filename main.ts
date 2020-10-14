f0f6af9dbf83c813245bea4c58bf8de0a6e231e4
410813072 <71751214+410813072@users.noreply.github.com>
2020-10-14 14:41:29 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
-input.onGesture(Gesture.LogoUp, function () {
-    basic.showNumber(1)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(0)
-    basic.pause(100)
-    basic.clearScreen()
-})
+for (let i = 0; i <= 9; i++) {
+    basic.showNumber(i)
+}
 basic.forever(function () {
 	
 })
