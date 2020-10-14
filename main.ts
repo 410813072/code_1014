462b0cc92edac3bfcc8975db902bc6f1366268ed
410813072 <71751214+410813072@users.noreply.github.com>
2020-10-14 14:22:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-input.calibrateCompass()
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showNumber(1)
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(0)
+    basic.pause(100)
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
